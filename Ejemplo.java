50674e8fa964603bdd3b2cd812073a99efb318b1
hz9-developer <68916041+hz9-developer@users.noreply.github.com>
2022-10-15 12:30:28 -0300

    Create Ejemplo.java

---
+++ Ejemplo.java
@@ -0,0 +1,17 @@
+import Animal; //Importamos la clase Animal para poder usarla
+
+public class Ejemplo
+{
+    public static void main(String[] args)
+    {
+        //Creamos un animal cuyo nombré será Falco
+        Animal miAnimal = new Animal("Falco");
+        //Le establecemos 3 años de edad a Falco.
+        miAnimal.setEdad(3);
+        //Mostraremos el nombre del animal por pantalla
+        System.out.println("El nombre es: " + miAnimal.getNombre());
+        //Mostramos la edad del animal por pantalla
+        System.out.println(" y tiene " + miAnimal.getEdad() + " años");
+        //Este código deberia imprimir "El nombre es: Falco y tiene 3 años"
+    }
+}
